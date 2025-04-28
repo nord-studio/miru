2fe2de3b3893e4aabd7cd3af7e44bd0fec33e325
Ty Mason <hi@tygr.dev>
2025-04-28 02:55:30 +0100

    fix: remove useless replace in middleware

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -5,37 +5,42 @@ import { NextRequest, NextResponse } from "next/server";
 type Session = typeof auth.$Infer.Session;
 
 export async function checkSession(request: NextRequest) {
-	const { data: session } = await betterFetch<Session>("/api/auth/get-session", {
-		baseURL: request.nextUrl.origin.replace("https", process.env.APP_ENV === "development" ? "http" : "https"),
-		headers: {
-			cookie: request.headers.get("cookie") || "",
-		},
-	});
+  const { data: session } = await betterFetch<Session>(
+    "/api/auth/get-session",
+    {
+      baseURL: request.nextUrl.origin,
+      headers: {
+        cookie: request.headers.get("cookie") || "",
+      },
+    },
+  );
 
-	if (!session) {
-		return NextResponse.redirect(new URL("/auth/login", request.url));
-	}
+  if (!session) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
 }
 
 export async function middleware(request: NextRequest) {
-	const url = request.nextUrl.clone();
+  const url = request.nextUrl.clone();
 
-	const hostname = request.headers.get("host");
-	const searchParams = request.nextUrl.searchParams.toString();
-	const path = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ""}`;
-	const appDomain = process.env.APP_DOMAIN ?? "localhost:3000";
+  const hostname = request.headers.get("host");
+  const searchParams = request.nextUrl.searchParams.toString();
+  const path = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ""}`;
+  const appDomain = process.env.APP_DOMAIN ?? "localhost:3000";
 
-	if (hostname === appDomain && path.startsWith("/admin")) {
-		await checkSession(request);
-		return NextResponse.next();
-	}
+  if (hostname === appDomain && path.startsWith("/admin")) {
+    await checkSession(request);
+    return NextResponse.next();
+  }
 
-	return NextResponse.rewrite(new URL(`/status-page/${hostname}${path}`, request.url));
+  return NextResponse.rewrite(
+    new URL(`/status-page/${hostname}${path}`, request.url),
+  );
 }
 
 export const config = {
-	matcher: [
-		// Exclude /api/*, /_next/*, /_static/*, /auth/*, /join/* and static files
-		"/((?!api/|_next/|auth|join|_static/[\\w-]+\\.\\w+).*)",
-	],
+  matcher: [
+    // Exclude /api/*, /_next/*, /_static/*, /auth/*, /join/* and static files
+    "/((?!api/|_next/|auth|join|_static/[\\w-]+\\.\\w+).*)",
+  ],
 };
